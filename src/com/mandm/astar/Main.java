d0e6e849cb25ed54dfb3e77ce3b1dccf9e2c6688
mfink9983 <martinfink99@gmail.com>
2016-10-18 08:42:35 +0200

    Initial commit

---
+++ src/com/mandm/astar/Main.java
@@ -0,0 +1,14 @@
+package com.mandm.astar;
+
+/**
+ * Created on 18.10.2016.
+ *
+ * @author Martin
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("OKE");
+    }
+
+}
